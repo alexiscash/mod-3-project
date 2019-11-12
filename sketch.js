3fc3b652a519c4e3dd0ab9cfbc73c5f72926054e
alexiscash <alexiscash97@rocketmail.com>
2019-11-12 14:49:28 -0600

    commiting out of fear

--- sketch.js
+++ sketch.js
@@ -2,15 +2,17 @@ let video;
 let poseNet;
 let poses = [];
 let button;
+let current;
+let path;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  getDrawings().then( res => {
+      console.log(res);
+      current = res[res.length - 1].id;
+  });
+  createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
-//   video.size(width, height);
-//   button = createButton('capture');
-//   button.position(0, 500);
-//   button.mousePressed(capturePose);
   poseNet = ml5.poseNet(video, modelReady);
   poseNet.on('pose', function(results) {
     poses = results;
@@ -24,22 +26,15 @@ function modelReady() {
 function draw() {
   background(51);
   image(video, 0, 0, 640, 480);
-
-  // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints();
   drawSkeleton();
 }
 
-// A function to draw ellipses over the detected keypoints
 function drawKeypoints()  {
-  // Loop through all the poses detected
   for (let i = 0; i < poses.length; i++) {
-    // For each pose detected, loop through all the keypoints
     let pose = poses[i].pose;
     for (let j = 0; j < pose.keypoints.length; j++) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
       let keypoint = pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
         fill(52, 219, 235);
         noStroke();
@@ -49,12 +44,9 @@ function drawKeypoints()  {
   }
 }
 
-// A function to draw the skeletons
 function drawSkeleton() {
-  // Loop through all the skeletons detected
   for (let i = 0; i < poses.length; i++) {
     let skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
     for (let j = 0; j < skeleton.length; j++) {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
@@ -77,6 +69,7 @@ function keyPressed() {
     if (keyCode == 32) {
         capturePose();
     } else if (keyCode == ENTER) {
+        // printCurrent();
         postImage();
     } else if (keyCode == 70) {
         let fs = fullscreen();
@@ -86,11 +79,15 @@ function keyPressed() {
     }
 }
 
+function printCurrent() {
+    console.log(current);
+}
+
 function postImage() {
     const body = document.body;
     const br = document.createElement('br');
     const img = document.createElement('img');
-    let path;
+    let path = `posenet${++current}`;
 
     fetch('http://localhost:3000/api/v1/drawings', {
         method: "POST",
@@ -99,19 +96,41 @@ function postImage() {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            name: 'ayyy lmao',
-            data: 'ayy lmao',
-            user_id: 4
+            name: `${path}`,
+            data: `/Users/flatironstudent/Downloads/${path}.png`,
+            user_id: 5
         })
     })
     .then( res => res.json())
     .then( json => {
         console.log(json);
         path = `posenet${json.id}`
+        // displayPose(img, body, br);
         saveCanvas(path)
-        img.src = `/Users/flatironstudent/Downloads/${path}.png`;
-        img.width = 640;
-        body.append(br, img);
+        setTimeout( () => {
+            img.src = `/Users/flatironstudent/Downloads/${path}.png`;
+            img.width = 640;
+            body.append(br, img);
+        }, 3000);
+        
     });
+}
+
+async function getDrawings() {
+    const response = await fetch('http://localhost:3000/api/v1/drawings');
+    const json = await response.json();
+    return json;
+}
+
+async function savePose() {
+    saveCanvas(path);
+    return;
+}
+
+async function displayPose(img, body, br) {
+    await savePose();
+    img.src = `/Users/flatironstudent/Downloads/${path}.png`;
+    img.width = 640;
+    body.append(br, img);
 }
 
